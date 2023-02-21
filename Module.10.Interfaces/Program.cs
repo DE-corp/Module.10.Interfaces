7d508f85d044bd9f0b4d97b09612c7615658bb68
Evdokimov Dmitry <crazygoup777@gmail.com>
2023-02-21 18:54:01 +0300

    Задание 10.1.4: Реализовал интерфейс

--- Module.10.Interfaces/Program.cs
+++ Module.10.Interfaces/Program.cs
@@ -2,11 +2,42 @@
 
 namespace Module._10.Interfaces
 {
+    public class Manager : IManager
+    {
+        public void Create()
+        {
+
+        }
+
+        public void Read()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Delete()
+        {
+
+        }
+    }
+
+    public interface IManager
+    {
+        public void Create();
+        public void Read();
+        public void Update();
+        public void Delete();
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
         }
     }
 }
