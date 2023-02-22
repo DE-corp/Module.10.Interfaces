766f37e68c6a3674d3870e17dbae83a03c293b63
Evdokimov Dmitry <crazygoup777@gmail.com>
2023-02-22 20:53:14 +0300

    Задание 10.3.1: Множественная реализация интерфейсов

--- Module.10.Interfaces/Program.cs
+++ Module.10.Interfaces/Program.cs
@@ -2,27 +2,59 @@
 
 namespace Module._10.Interfaces
 {
-    public interface IWorker
+    public interface IFile
     {
-        void Build();
+        void ReadFile();
     }
 
-    public class Worker : IWorker
+    public interface IBinaryFile
     {
-        void IWorker.Build()
+        void ReadFile();
+        void OpenBinaryFile();
+    }
+
+    public interface IMailer
+    {
+        void SendMail();
+    }
+
+    class FileManager : IFile, IBinaryFile, IMailer
+    {
+        public void OpenBinaryFile()
         {
-            Console.WriteLine("Билд!");
+            Console.WriteLine("Открываем бинарный файл");
+        }
+
+        public void SendMail()
+        {
+            Console.WriteLine("Отправляем письмо");
+        }
+
+        void IFile.ReadFile()
+        {
+            Console.WriteLine("Читаем текстовый файл");
+        }
+
+        void IBinaryFile.ReadFile()
+        {
+            Console.WriteLine("Читаем бинарный файл");
         }
     }
+
 
     class Program
     {
         static void Main(string[] args)
         {
 
-            Worker worker = new Worker();
+            IFile file = new FileManager();
+            IBinaryFile binaryFile = new FileManager();
+            IMailer mailer = new FileManager();
 
-            ((IWorker)worker).Build();
+            file.ReadFile();
+            binaryFile.ReadFile();
+            binaryFile.OpenBinaryFile();
+            mailer.SendMail();
         }
     }
 }
